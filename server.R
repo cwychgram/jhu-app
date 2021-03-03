a723395836502cb364b9395476bf5d7b9cba4010
Cara Wychgram <cwychgr2@jhu.edu>
2021-03-03 14:01:52 -0500

    3-3-21

--- server.R
+++ server.R
@@ -5,7 +5,6 @@ server <- function(input, output, session) {
   source("sections/content_overview/casesMap_ca.R", local = TRUE)
   source("sections/content_overview/tableTop.R", local = TRUE)
   source("sections/content_overview/timePlots.R", local = TRUE)
-  source("sections/content_overview/timePlots_ca.R", local = TRUE)
   source("sections/content_data/dataMaps.R", local = TRUE)
   source("sections/content_data/dataMaps_ca.R", local = TRUE)
   # source("sections/content_profile/countyProfile.R", local = TRUE)
